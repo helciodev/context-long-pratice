e9c07c1d62cc76f2f8115d286178c87290dde4ec
helcio <h.andre@giap.pl>
2023-12-03 11:19:37 +0100

    Complete phase 2

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,11 +1,35 @@
 // Your code here
+class Calculator {
+  constructor() {
+    this.total = 0;
+  }
 
+  add(valueToSum) {
+    this.total += valueToSum;
+    return this.total;
+  }
+
+  subtract(value) {
+    this.total -= value;
+    return this.total;
+  }
+
+  multiply(value) {
+    this.total *= value;
+    return this.total;
+  }
+
+  divide(value) {
+    this.total /= value;
+    return this.total;
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = Calculator;
+  module.exports = Calculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
