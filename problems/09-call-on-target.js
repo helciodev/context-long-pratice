3064c4f2841713551c4a1fe3c23d05b910c21543
helcio <h.andre@giap.pl>
2023-12-03 20:21:48 +0100

    Complete phase 09

--- problems/09-call-on-target.js
+++ problems/09-call-on-target.js
@@ -1,7 +1,9 @@
 function callOnTarget(func, obj1, obj2) {
-  // Your code here
+  return func.call(obj1, obj2);
 }
-
+function greet(other) {
+  return "I'm " + this.name + ". Nice to meet you, " + other.name;
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
