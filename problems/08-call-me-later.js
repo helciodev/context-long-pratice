90e90a8760cc93deda90b1fa246d21f5be259e79
helcio <h.andre@giap.pl>
2023-12-03 20:16:03 +0100

    Complete phase 08

--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,11 +1,24 @@
-// Your code here
+class CallCenter {
+  constructor(name) {
+    this.name = name;
+  }
 
+  sayHello() {
+    console.log(`Hello this is ${this.name}`);
+  }
+
+  callMeLater = (time) => {
+    setTimeout(() => {
+      console.log(`Hello this is ${this.name}`);
+    }, time);
+  };
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = CallCenter;
+  module.exports = CallCenter;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
