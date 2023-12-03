53266179b9d118a54f6fe565ef9583aadfe0976b
helcio <h.andre@giap.pl>
2023-12-03 12:10:11 +0100

    Complete phase 3

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,11 +1,22 @@
-// Your code here
-
+class Dog {
+  static makeJet() {
+    return {
+      name: "Jet",
+      changeName(newName) {
+        this.name = newName;
+      },
+      speak(word) {
+        return `${this.name} says ${word}`;
+      },
+    };
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = Dog;
+  module.exports = Dog;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
