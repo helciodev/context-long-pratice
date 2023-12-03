37ebdf6314c60c8ef721e384297af03d0577abcb
helcio <h.andre@giap.pl>
2023-12-03 19:44:55 +0100

    Complete phase 6

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,10 +1,30 @@
-// Your code here
+const Calculator = require("./02-calculator");
+
+class FancyCalculator extends Calculator {
+  constructor(total = 0) {
+    super(total);
+  }
+  setTotal(newTotal) {
+    this.total = newTotal;
+    return this.total;
+  }
+
+  squared() {
+    this.total *= this.total;
+    return this.total;
+  }
+
+  modulo(num) {
+    this.total %= num;
+    return this.total;
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = FancyCalculator;
+  module.exports = FancyCalculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
